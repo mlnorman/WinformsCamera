6d51c5b6ba8189a6b4717be9632da2180d50923a
Michael Norman <MichaelNorman@packagingcorp.com>
2020-07-01 13:53:53 -0400

    initial

---
+++ Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnRun = New Infragistics.Win.Misc.UltraButton()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.picBox = New System.Windows.Forms.PictureBox()
+        Me.txtBarcode = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSnapshot = New Infragistics.Win.Misc.UltraButton()
+        Me.pbScreenshot = New System.Windows.Forms.PictureBox()
+        Me.btnSaveScreenshot = New Infragistics.Win.Misc.UltraButton()
+        CType(Me.picBox,System.ComponentModel.ISupportInitialize).BeginInit
+        CType(Me.pbScreenshot,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'btnRun
+        '
+        Me.btnRun.Location = New System.Drawing.Point(230, 581)
+        Me.btnRun.Name = "btnRun"
+        Me.btnRun.Size = New System.Drawing.Size(75, 23)
+        Me.btnRun.TabIndex = 1
+        Me.btnRun.Text = "Start"
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = true
+        Me.BackgroundWorker1.WorkerSupportsCancellation = true
+        '
+        'picBox
+        '
+        Me.picBox.Location = New System.Drawing.Point(12, 12)
+        Me.picBox.Name = "picBox"
+        Me.picBox.Size = New System.Drawing.Size(581, 506)
+        Me.picBox.TabIndex = 3
+        Me.picBox.TabStop = false
+        '
+        'txtBarcode
+        '
+        Me.txtBarcode.Location = New System.Drawing.Point(433, 585)
+        Me.txtBarcode.Name = "txtBarcode"
+        Me.txtBarcode.Size = New System.Drawing.Size(175, 20)
+        Me.txtBarcode.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(380, 588)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Barcode"
+        '
+        'btnSnapshot
+        '
+        Me.btnSnapshot.Location = New System.Drawing.Point(703, 581)
+        Me.btnSnapshot.Name = "btnSnapshot"
+        Me.btnSnapshot.Size = New System.Drawing.Size(75, 23)
+        Me.btnSnapshot.TabIndex = 7
+        Me.btnSnapshot.Text = "Snapshot"
+        '
+        'pbScreenshot
+        '
+        Me.pbScreenshot.Location = New System.Drawing.Point(599, 12)
+        Me.pbScreenshot.Name = "pbScreenshot"
+        Me.pbScreenshot.Size = New System.Drawing.Size(581, 506)
+        Me.pbScreenshot.TabIndex = 8
+        Me.pbScreenshot.TabStop = false
+        '
+        'btnSaveScreenshot
+        '
+        Me.btnSaveScreenshot.Location = New System.Drawing.Point(879, 581)
+        Me.btnSaveScreenshot.Name = "btnSaveScreenshot"
+        Me.btnSaveScreenshot.Size = New System.Drawing.Size(108, 23)
+        Me.btnSaveScreenshot.TabIndex = 9
+        Me.btnSaveScreenshot.Text = "Save Screenshot"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1191, 641)
+        Me.Controls.Add(Me.btnSaveScreenshot)
+        Me.Controls.Add(Me.pbScreenshot)
+        Me.Controls.Add(Me.btnSnapshot)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtBarcode)
+        Me.Controls.Add(Me.picBox)
+        Me.Controls.Add(Me.btnRun)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.picBox,System.ComponentModel.ISupportInitialize).EndInit
+        CType(Me.pbScreenshot,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+    Friend WithEvents btnRun As Infragistics.Win.Misc.UltraButton
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents picBox As PictureBox
+    Friend WithEvents txtBarcode As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSnapshot As Infragistics.Win.Misc.UltraButton
+    Friend WithEvents pbScreenshot As PictureBox
+    Friend WithEvents btnSaveScreenshot As Infragistics.Win.Misc.UltraButton
+End Class
---
+++ Form1.vb
@@ -0,0 +1,103 @@
+﻿
+Imports System.ComponentModel
+Imports System.Drawing.Imaging
+Imports System.IO
+Imports System.Threading
+Imports Infragistics.Win.Misc
+Imports Infragistics.Win.UltraWinEditors
+Imports OpenCvSharp
+Imports OpenCvSharp.Extensions
+Imports ZXing
+Imports ZXing.Common
+
+Public Class Form1
+    Private Shadows capture As VideoCapture
+    Private frame As Mat
+    Private image As Bitmap
+    Private camera As Thread
+    Private isCameraRunning As Boolean
+
+    Public Sub New()
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+    End Sub
+
+    Private Sub CaptureCameraCallback()
+
+        frame = New Mat()
+        capture = New VideoCapture(0)
+        capture.Open(0)
+
+        If capture.IsOpened() Then
+            While isCameraRunning
+
+                capture.Read(frame)
+                image = BitmapConverter.ToBitmap(frame)
+                If picBox.Image IsNot Nothing Then
+                    picBox.Image.Dispose()
+                End If
+                picBox.Image = image
+            End While
+        End If
+    End Sub
+
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
+        If String.Equals(btnRun.Text, "Start") Then
+            BackgroundWorker1.RunWorkerAsync()
+            btnRun.Text = "Stop"
+            isCameraRunning = True
+        ElseIf String.Equals(btnRun.Text, "Stop") Then
+            isCameraRunning = False
+            capture.Release()
+            BackgroundWorker1.CancelAsync()
+        End If
+    End Sub
+
+    Private Sub btnSnapshot_Click(sender As Object, e As EventArgs) Handles btnSnapshot.Click
+        If isCameraRunning Then
+            Try
+
+                'Get Parameter not valid exceptions because this camera is running in a background worker
+                ' thats breaking debug.
+
+                Dim snap = new Bitmap(DirectCast(picBox.Image, Image))
+                pbScreenshot.Image = snap
+
+                ' Saves the image if you want...
+                'Dim myGuid = Guid.NewGuid()
+                'Dim filename = String.Format("{0}/pictures/{1}.png", System.Environment.GetEnvironmentVariable("USERPROFILE"), myGuid)
+                'snap.Save(filename, ImageFormat.PNG)
+
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+
+        Else
+            MessageBox.Show("The camera is not capturing...")
+        End If
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        CaptureCameraCallback()
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        capture.Release()
+        btnRun.Text = "Start"
+        isCameraRunning = False
+        picBox.Image = Nothing
+    End Sub
+
+    Private Sub btnSaveScreenshot_Click(sender As Object, e As EventArgs) Handles btnSaveScreenshot.Click
+        If pbScreenshot.Image IsNot Nothing Then
+            Dim map = new Bitmap(pbScreenshot.Image)
+            Dim myGuid = Guid.NewGuid()
+            Dim filename = String.Format("{0}/pictures/{1}.png", System.Environment.GetEnvironmentVariable("USERPROFILE"), myGuid)
+            map.Save(filename, ImageFormat.PNG)
+        Else 
+            MessageBox.Show("No snapshot has been taken...")
+        End If
+    End Sub
+End Class
