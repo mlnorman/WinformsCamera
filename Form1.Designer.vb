d68e1b0225b7e8acfe3489a23d070ec19516542d
Michael Norman <MichaelNorman@packagingcorp.com>
2020-09-30 11:12:45 -0400

    added camera flip and set image size

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -28,6 +28,8 @@ Partial Class Form1
         Me.btnRun = New System.Windows.Forms.Button()
         Me.btnScreenshot = New System.Windows.Forms.Button()
         Me.btnSaveScreenshot = New System.Windows.Forms.Button()
+        Me.btnFlipCamera = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.picBox,System.ComponentModel.ISupportInitialize).BeginInit
         CType(Me.pbScreenshot,System.ComponentModel.ISupportInitialize).BeginInit
         Me.SuspendLayout
@@ -41,7 +43,8 @@ Partial Class Form1
         '
         Me.picBox.Location = New System.Drawing.Point(12, 12)
         Me.picBox.Name = "picBox"
-        Me.picBox.Size = New System.Drawing.Size(581, 506)
+        Me.picBox.Size = New System.Drawing.Size(581, 470)
+        Me.picBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picBox.TabIndex = 3
         Me.picBox.TabStop = false
         '
@@ -49,7 +52,8 @@ Partial Class Form1
         '
         Me.pbScreenshot.Location = New System.Drawing.Point(599, 12)
         Me.pbScreenshot.Name = "pbScreenshot"
-        Me.pbScreenshot.Size = New System.Drawing.Size(581, 506)
+        Me.pbScreenshot.Size = New System.Drawing.Size(581, 470)
+        Me.pbScreenshot.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.pbScreenshot.TabIndex = 8
         Me.pbScreenshot.TabStop = false
         '
@@ -80,11 +84,31 @@ Partial Class Form1
         Me.btnSaveScreenshot.Text = "Save Screenshot"
         Me.btnSaveScreenshot.UseVisualStyleBackColor = true
         '
+        'btnFlipCamera
+        '
+        Me.btnFlipCamera.Location = New System.Drawing.Point(12, 578)
+        Me.btnFlipCamera.Name = "btnFlipCamera"
+        Me.btnFlipCamera.Size = New System.Drawing.Size(90, 23)
+        Me.btnFlipCamera.TabIndex = 13
+        Me.btnFlipCamera.Text = "Flip Camera"
+        Me.btnFlipCamera.UseVisualStyleBackColor = true
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(12, 619)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(43, 13)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Camera"
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6!, 13!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1191, 641)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnFlipCamera)
         Me.Controls.Add(Me.btnSaveScreenshot)
         Me.Controls.Add(Me.btnScreenshot)
         Me.Controls.Add(Me.btnRun)
@@ -95,6 +119,7 @@ Partial Class Form1
         CType(Me.picBox,System.ComponentModel.ISupportInitialize).EndInit
         CType(Me.pbScreenshot,System.ComponentModel.ISupportInitialize).EndInit
         Me.ResumeLayout(false)
+        Me.PerformLayout
 
 End Sub
     Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
@@ -103,4 +128,6 @@ End Sub
     Friend WithEvents btnRun As Button
     Friend WithEvents btnScreenshot As Button
     Friend WithEvents btnSaveScreenshot As Button
+    Friend WithEvents btnFlipCamera As Button
+    Friend WithEvents Label1 As Label
 End Class
--- Form1.vb
+++ Form1.vb
@@ -3,6 +3,7 @@ Imports System.ComponentModel
 Imports System.Drawing.Imaging
 Imports System.IO
 Imports System.Threading
+Imports Infragistics.Win
 Imports Infragistics.Win.Misc
 Imports Infragistics.Win.UltraWinEditors
 Imports OpenCvSharp
@@ -16,56 +17,78 @@ Public Class Form1
     Private image As Bitmap
     Private camera As Thread
     Private isCameraRunning As Boolean
+    Private camera_id As Integer
 
     Public Sub New()
         ' This call is required by the designer.
         InitializeComponent()
 
         ' Add any initialization after the InitializeComponent() call.
+        Label1.Text = "Camera " & camera_id
     End Sub
 
     Private Sub CaptureCameraCallback()
 
-        frame = New Mat()
-        capture = New VideoCapture(0)
-        capture.Open(0)
+        dim initalCameraId = camera_id
 
+        if isCameraRunning = False Then isCameraRunning = true
+
+        frame = new Mat()
+        'frame = new Mat(new Size(1024, 768), MatType.CV_64FC3)
+        capture = New VideoCapture(CaptureDevice.MSMF, camera_id)
+        'frame =capture.RetrieveMat() 
+        'frame.Resize(new Size(1920,1080))
+
+
+        capture.Open(camera_id,CaptureDevice.MSMF)
+        capture.Set(CaptureProperty.FrameHeight, 1080)
+        capture.Set(CaptureProperty.FrameWidth, 1440)
+        
         If capture.IsOpened() Then
             While isCameraRunning
 
                 capture.Read(frame)
-                if isCameraRunning Then
-                    image = BitmapConverter.ToBitmap(frame)
-                Else 
+                If isCameraRunning Then
+                    image = frame.ToBitmap()
+                    'image.SetResolution(1440, 1080)
+                Else
                     image = Nothing
                 End If
                 If picBox.Image IsNot Nothing Then
                     picBox.Image.Dispose()
                 End If
                 picBox.Image = image
+
+                if camera_id <> initalCameraId Then
+                    isCameraRunning = false
+                    capture.Dispose()
+                    CaptureCameraCallback()
+                end If
             End While
         End If
     End Sub
 
-    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click 
+    Private Sub btnRun_Click(sender As Object, e As EventArgs) Handles btnRun.Click
         If String.Equals(btnRun.Text, "Start") Then
             BackgroundWorker1.RunWorkerAsync()
             btnRun.Text = "Stop"
             isCameraRunning = True
+            'btnFlipCamera.Enabled = False
         ElseIf String.Equals(btnRun.Text, "Stop") Then
             isCameraRunning = False
+            'btnFlipCamera.Enabled = True
             BackgroundWorker1.CancelAsync()
         End If
     End Sub
 
-    Private Sub btnSnapshot_Click(sender As Object, e As EventArgs) Handles btnScreenshot.Click 
+    Private Sub btnSnapshot_Click(sender As Object, e As EventArgs) Handles btnScreenshot.Click
         If isCameraRunning Then
             Try
 
                 'Get Parameter not valid exceptions because this camera is running in a background worker
                 ' thats breaking debug.
 
-                Dim snap = new Bitmap(DirectCast(picBox.Image, Image))
+                Dim snap = frame.ToBitmap()
                 pbScreenshot.Image = snap
 
                 ' Saves the image if you want...
@@ -91,18 +114,44 @@ Public Class Form1
         isCameraRunning = False
         capture.Release()
         btnRun.Text = "Start"
-        
+
         picBox.Image = Nothing
     End Sub
 
-    Private Sub btnSaveScreenshot_Click(sender As Object, e As EventArgs) Handles btnSaveScreenshot.Click 
+    Private Sub btnSaveScreenshot_Click(sender As Object, e As EventArgs) Handles btnSaveScreenshot.Click
         If pbScreenshot.Image IsNot Nothing Then
-            Dim map = new Bitmap(pbScreenshot.Image)
+            Dim map = ResizeImage(pbScreenshot.Image)
+
             Dim myGuid = Guid.NewGuid()
             Dim filename = String.Format("{0}/pictures/{1}.png", System.Environment.GetEnvironmentVariable("USERPROFILE"), myGuid)
-            map.Save(filename, ImageFormat.PNG)
-        Else 
+            map.Save(filename, ImageFormat.Png)
+        Else
             MessageBox.Show("No snapshot has been taken...")
         End If
     End Sub
+
+    Private Function ResizeImage(img As Bitmap) As Bitmap
+        Dim imgMat = img.ToMat()
+        ' imgMat = imgMat.Resize(new Size(1024, 768),fx := .5, fy :=.5)
+        Return imgMat.ToBitmap()
+    End Function
+
+    Private Sub btnFlipCamera_Click(sender As Object, e As EventArgs) Handles btnFlipCamera.Click
+        'Dim wasRunning = False
+        'If String.Equals(btnRun.Text, "Stop") Then
+        '    isCameraRunning = False
+        '    BackgroundWorker1.CancelAsync()
+        '    wasRunning = True
+        'End If
+
+        if camera_id = 1 Then
+            camera_id  = 0
+        else
+            camera_id = 1
+        end if
+        Label1.Text = "Camera " & camera_id
+
+    End Sub
+
+   
 End Class
